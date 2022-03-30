bf48c8d5aede9baaa8c9063ad6f451c28859214f
Francois Groleau <francois.groleau@gmail.com>
2022-03-29 20:32:29 -0400

    feat add support for changelog (#5)

--- release.config.js
+++ release.config.js
@@ -4,4 +4,32 @@ module.exports = {
     'main',
     'beta',
   ],
+  verifyConditions: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+  ],
+  prepare: [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    {
+      path: '@semantic-release/git',
+      assets: ['package.json', 'CHANGELOG.md', 'README.md'],
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: [
+          'package.json',
+        ],
+        // eslint-disable-next-line no-template-curly-in-string
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
+      },
+    ],
+  ],
 }
